1e393128d8248c93ae75709d4964031bba96726e
Giselle Ribeiro <giselle_ribeiro@outlook.com.br>
2023-08-28 23:05:04 -0300

    renomeando classes

---
+++ expressbank/expressbank_herdado/src/Engenheiro.java
@@ -0,0 +1,10 @@
+//Manager é uma extensao de  Employee
+public class Engenheiro extends Funcionario { 
+  
+   
+     public double getbenefit() {
+        System.out.println("Chamando o método de bonificação do engenheiro: ");
+        return 100;
+
+    }
+}
